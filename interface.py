d80574156e75d1542c5b72b5000aaa28b5962b11
Baptiste Morin <b.a.p.t.s.t.e7@gmail.com>
2023-01-10 01:44:36 +0100

    Mise en place de bandes blanches : trous fixes, à largeur et à fréquence personnalisables Possibilité de mixer les types de corruption de signal

--- interface.py
+++ interface.py
@@ -47,21 +47,71 @@ def ask_phases():
     return Z
 
 def ask_holes(nb):
-    T = 0
+    choice = choose()
+    a = b = c = 0
+    if choice%2==0 :
+        #trous aléatoires
+        a = randoms(nb)
+    if choice>2 :
+        #trous fixes
+        b, c = predictables(nb)
+    return a, b, c
+
+def choose():
+    chosen = 0
     s = ''
-    while T==0 or T>nb or 0>T :
+    while not (0<chosen<5) :
         try:
-            answer = input("{}Saisissez le nombre entier de trous que vous souhaitez insérer.\nNOMBRE TOTAL D'ÉCHANTILLONS : {}\n".format(s, nb))
-            T = int(answer)
+            answer = input("{}Comment voulez-vous corrompre le signal ? Répondre le numéro correspondant à la réponse.\n\n1 - Ne pas corrompre le signal.\n2 - Corrompre le signal par des trous aléatoires.\n3 - Corrompre le signal par un nombre de trous fixes.\n4 - Corrompre le signal par un mélange de trous aléatoires et de trous fixes.".format(s))
+            chosen = int(answer)
         except ValueError:
             if (answer==''):
                 print("Le signal ne sera pas corrompu.")
                 return 0
         finally:
-            s = 'Vous devez nécessairement entrer un nombre entier en chiffres, supérieur ou égal à 1, mais inférieur au nombre total d\'échantillons. Exemples : {} ou {}\n'.format(int(nb/3 +1), int(nb/1.4 +1))
+            s = 'Vous devez nécessairement entrer un entier correspondant à l\'une des 4 propositions'
+    return chosen
+
+#fonction répartissant les trous aléatoirement
+def randoms(nb):
+    T = -1
+    while not (0<T<nb) :
+        try:
+            answer = input("{}Saisissez le nombre entier de trous aléatoires que vous souhaitez insérer.\nNOMBRE TOTAL D'ÉCHANTILLONS : {}\n".format(s, nb))
+            T = int(answer)
+        except ValueError:
+            pass
+        finally:
+            s = 'Vous devez nécessairement entrer un nombre entier en chiffres, supérieur ou égal à 1, mais inférieur au nombre total d\'échantillons. Exemples : {} ou {}\n'.format(int(nb/3)+1, int(nb/1.4)+1)
     if (T==1):
         s = ''
     else:
         s = 's'
     print("On corrompt le signal ({} points) avec {} trou{}.".format(nb, T, s))
     return T
+
+#fonction répartissant les trous d'une certaine largeur espacés d'un certain intervalle
+def predictables(nb):
+    #on demande la largeur des trous
+    width = 0
+    s = ''
+    while not (0<width<nb//4) :
+        try:
+            answer = input("{}Saisissez la largeur des bandes blanches que vous souhaitez insérer.\nLARGEUR MAXIMALE DES TROUS : {}\n".format(s, nb/4))
+            width = int(answer)
+        except ValueError:
+            pass
+        finally:
+            s = 'Vous devez nécessairement entrer un nombre entier en chiffres, supérieur ou égal à 1, mais inférieur à la longueur maximale d\'un trou (équivalente à un quart de la longueur totale de mesure). Exemples : {} ou {}\n'.format(int(nb/12)+1, int(nb/5.6)+1)
+    #on demande le nombre de trous ou leur fréquence d'apparition ? Le deuxième choix semble plus indiqué, car il permet de donner les mêmes trous à différents échantillons de mesure de longueur différent (3 et 7.5 périodes par exemple)
+    freq = 0
+    s = ''
+    while not (width<freq<=nb-width) :
+        try:
+            answer = input("{}Saisissez la fréquence à vous souhaitez insérer vos bandes blanches (Tous les X trous).\nFréquence maximale : une bande tous les {} trous\nFréquence minimale (pour avoir 2 bandes): une bande tous les {} trous".format(s, width+1, nb-width))
+            freq = int(answer)
+        except ValueError:
+            pass
+        finally:
+            s = 'Vous devez nécessairement entrer un nombre entier en chiffres, supérieur ou égal à 1, mais inférieur au nombre total d\'échantillons.'
+    return width, freq
