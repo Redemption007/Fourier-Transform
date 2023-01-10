9c75f6ec0cef535e55394d08332de82902cb5ebf
Baptiste Morin <b.a.p.t.s.t.e7@gmail.com>
2023-01-10 16:01:50 +0100

    Ajout de bandes blanches personnalisables en tant que trous récurrents (dus par ex au cycle jour/nuit)

--- corrupteur.py
+++ corrupteur.py
@@ -1,11 +1,25 @@
-from numpy import nonzero, delete
+from numpy import nonzero, delete, tile
 from random import random
 
-def corruption(func, nb_trous, x):
+def corruption(func, x, nb_trous=0, width=0, freq=0):
     corr = temp = func(x)
-    while (corr.size-temp.size<nb_trous):
-        index1 = int(random()*temp.size)
-        index2 = nonzero(corr == temp[index1])[0][0]
-        corr[index2] = 0
-        temp = delete(temp, index1)
+    if width :
+        index = 0
+        while index<corr.size :
+            if index+width>corr.size :
+                #on remplace le nombre restant de points par des zéros
+                hole = tile(0, corr.size-index)
+                corr[index:] = hole
+                continue
+            #on remplace le nombre width de points par 0
+            hole = tile(0, width)
+            corr[index:index+width] = hole
+            index+=freq
+    if nb_trous :
+        while (corr.size-temp.size<nb_trous):
+            index1 = int(random()*temp.size)
+            index2 = nonzero(corr == temp[index1])[0][0]
+            corr[index2] = 0
+            temp = delete(temp, index1)
     return corr
+    
